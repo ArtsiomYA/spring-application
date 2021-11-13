9d40c39e544489835bbcf6b43d34cc88b45d6f64
Artsiom Yahorau <a.yahorau@andersenlab.com>
2021-11-13 19:12:52 +0300

    Create FacultyDto class

--- src/main/java/com/example/practics/dto/FacultyDto.java
+++ src/main/java/com/example/practics/dto/FacultyDto.java
@@ -1,4 +1,12 @@
 package com.example.practics.dto;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
 public class FacultyDto {
+
+    @NotNull
+    private String admission_plan;
 }
